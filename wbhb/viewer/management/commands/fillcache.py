ac37e0b7aaa58dde127a5a13682d435fca91b38c
Bryan Tarpley <bptarpley@tamu.edu>
2023-05-22 15:04:09 -0500

    specifying whitenoise version; fixing "fillcache" management command so that it adheres to newer practices for django template rendering

--- wbhb/viewer/management/commands/fillcache.py
+++ wbhb/viewer/management/commands/fillcache.py
@@ -1,7 +1,7 @@
 import os
 import json
 from django.core.management.base import BaseCommand, CommandError
-from django.template import loader, Context
+from django.template import loader  #, Context
 from wbhb.viewer.models import *
 
 
@@ -61,7 +61,7 @@ class Command(BaseCommand):
             json.dump(sources_json, fout)
 
         template = loader.get_template('export.txt')
-        context = Context({'sources': sources})
+        #context = Context({'sources': sources})
 
         with open('export.txt', 'w') as fout:
-            fout.write(template.render(context).replace('\n', '\r\n'))
+            fout.write(template.render({'sources': sources}).replace('\n', '\r\n'))
